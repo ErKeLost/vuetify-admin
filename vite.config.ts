b3256a4ccd00418200222067e7cc51e5bb6e8d76
ERKELSOT <1256029807@qq.com>
2022-12-18 07:43:38 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,56 @@
+// Plugins
+import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
+
+// Utilities
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    vuetify({
+      autoImport: true,
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "safari-pinned-tab.svg"],
+      manifest: {
+        name: "ViteTemplate",
+        short_name: "ViteTemplate",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+  define: { "process.env": {} },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
+  },
+  server: {
+    port: 1256,
+  },
+});
