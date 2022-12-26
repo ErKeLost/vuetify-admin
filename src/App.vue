e1a33f25d2c9de38b0af7826e944f8b0ae365017
ERKELSOT <1256029807@qq.com>
2022-12-26 20:47:29 +0800

    chore: test

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@
           <v-btn rounded="pill" @click="changeTheme('dark')" color="primary">
             dark Button
           </v-btn>
-          <v-color-picker v-model="color"></v-color-picker>
+          <v-color-picker v-model="color" mode="rgb"></v-color-picker>
           {{ color }}
         </v-navigation-drawer>
       </v-main>
@@ -43,7 +43,7 @@
 </template>
 
 <script setup lang="ts">
-// import { useTheme } from "vuetify";
+import { useTheme } from "vuetify";
 import { resolve } from "node:path";
 import { useVuetify } from "./composable/vuetify";
 import { useSettingStore } from "./store/modules/setting";
@@ -53,20 +53,24 @@ import { useThemeStore } from "./store/modules/theme";
 //   theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
 // }
 // const vuetify = useVuetify();
-const instance = getCurrentInstance();
-nextTick(() => {
-  console.log(instance?.root?.ctx!.$vuetify.theme.current.colors.primary);
-  const theme = instance?.root?.ctx!.$vuetify.theme.current;
-  console.log(theme);
-  theme.colors.primary = "#abf";
-  console.log(theme);
-});
+// const instance = getCurrentInstance();
+// nextTick(() => {
+//   console.log(instance?.root?.ctx!.$vuetify.theme.current.colors.primary);
+//   const theme = instance?.root?.ctx!.$vuetify.theme.current;
+//   console.log(theme);
+//   theme.colors.primary = "#abf";
+//   console.log(theme);
+// });
+const theme = useTheme();
+
 const color = ref("");
 watch(color, (newValue) => {
   const docEle = document.documentElement;
   docEle.classList.add("v-theme--light");
-  docEle.style.setProperty("--v-theme-primary", "200,156,132");
+  docEle.style.setProperty("--v-theme-primary", newValue);
   console.log(docEle);
+  document.querySelector("button")?.classList.remove("v-theme--light");
+  console.log(document.querySelector("button"));
 
   // console.log(document.documentElement);
   // const theme = instance?.root?.ctx!.$vuetify.theme.current;
@@ -81,56 +85,7 @@ const { changeTheme } = useThemeStore();
 const drawer = ref(false);
 </script>
 <style>
-.v-theme--light {
-  color-scheme: normal;
-  --v-theme-background: 255, 255, 255;
-  --v-theme-background-overlay-multiplier: 1;
-  --v-theme-surface: 255, 255, 255;
-  --v-theme-surface-overlay-multiplier: 1;
-  --v-theme-surface-variant: 66, 66, 66;
-  --v-theme-surface-variant-overlay-multiplier: 2;
-  --v-theme-on-surface-variant: 238, 238, 238;
-  --v-theme-primary: 98, 0, 238;
-  --v-theme-primary-overlay-multiplier: 2;
-  --v-theme-primary-darken-1: 55, 0, 179;
-  --v-theme-primary-darken-1-overlay-multiplier: 2;
-  --v-theme-secondary: 3, 218, 198;
-  --v-theme-secondary-overlay-multiplier: 1;
-  --v-theme-secondary-darken-1: 1, 135, 134;
-  --v-theme-secondary-darken-1-overlay-multiplier: 1;
-  --v-theme-error: 176, 0, 32;
-  --v-theme-error-overlay-multiplier: 2;
-  --v-theme-info: 33, 150, 243;
-  --v-theme-info-overlay-multiplier: 1;
-  --v-theme-success: 76, 175, 80;
-  --v-theme-success-overlay-multiplier: 1;
-  --v-theme-warning: 251, 140, 0;
-  --v-theme-warning-overlay-multiplier: 1;
-  --v-theme-on-background: 0, 0, 0;
-  --v-theme-on-surface: 0, 0, 0;
-  --v-theme-on-primary: 255, 255, 255;
-  --v-theme-on-primary-darken-1: 255, 255, 255;
-  --v-theme-on-secondary: 0, 0, 0;
-  --v-theme-on-secondary-darken-1: 255, 255, 255;
-  --v-theme-on-error: 255, 255, 255;
-  --v-theme-on-info: 255, 255, 255;
-  --v-theme-on-success: 255, 255, 255;
-  --v-theme-on-warning: 255, 255, 255;
-  --v-border-color: 0, 0, 0;
-  --v-border-opacity: 0.12;
-  --v-high-emphasis-opacity: 0.87;
-  --v-medium-emphasis-opacity: 0.6;
-  --v-disabled-opacity: 0.38;
-  --v-idle-opacity: 0.04;
-  --v-hover-opacity: 0.04;
-  --v-focus-opacity: 0.12;
-  --v-selected-opacity: 0.08;
-  --v-activated-opacity: 0.12;
-  --v-pressed-opacity: 0.12;
-  --v-dragged-opacity: 0.08;
-  --v-theme-kbd: 33, 37, 41;
-  --v-theme-on-kbd: 255, 255, 255;
-  --v-theme-code: 245, 245, 245;
-  --v-theme-on-code: 0, 0, 0;
+:root {
+  --v-theme-primary: "#f53f3f" !important;
 }
 </style>
